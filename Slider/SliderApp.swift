325668d8db3f00b95f7e687b624ca59601b0e18a
Joacim Nidén <joacim.niden@oneagency.se>
2023-05-06 19:59:17 +0200

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Joacim Nidén on 2023-05-06.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
